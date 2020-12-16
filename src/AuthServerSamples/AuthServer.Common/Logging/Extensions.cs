127a04ee2d9bd7ceda7b49a0b238540963864b76
Vadim <SadrievVF@it2g.ru>
2020-12-16 23:06:52 +0500

    Add ef core auth web and common projects. add logging

---
+++ src/AuthServerSamples/AuthServer.Common/Logging/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Exceptions;
+
+namespace AuthServer.Common.Logging
+{
+    /// <summary>
+    /// Extends method for logging
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Replaces all logging providers with serilog logging provider
+        /// </summary>
+        public static IServiceCollection AddSerilog(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services.AddLogging(builder =>
+             {
+                 builder.ClearProviders();
+                 builder.AddSerilog(
+                     new LoggerConfiguration()
+                     .ReadFrom.Configuration(configuration.GetSection("Logging"))
+                     .Enrich.WithExceptionDetails()
+                     .CreateLogger());
+             });
+        }
+    }
+}
---
+++ src/AuthServerSamples/AuthServerEfCore.Web/Startup.cs
@@ -0,0 +1,53 @@
+using AuthServer.Common.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AuthServerEfCore.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IWebHostEnvironment Environment { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSerilog(Configuration);
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
