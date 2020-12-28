2829e6846d4f0c52d894f40b9e7d34461129b6a0
Садриев Вадим <SadrievVF@it2g.ru>
2020-12-28 18:07:08 +0500

    Add Docker

---
+++ src/AuthServerSamples/AuthServerEfCore.Web/Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServerEfCore.Web.Controllers
+{
+    [Route("hello")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> World() => Ok("World");
+    }
+}
