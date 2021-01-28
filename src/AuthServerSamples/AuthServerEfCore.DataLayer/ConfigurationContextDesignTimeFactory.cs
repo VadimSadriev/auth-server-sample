f807f100b3c036537d7837ba02efff69ec928f33
Садриев Вадим <SadrievVF@it2g.ru>
2021-01-28 17:57:36 +0500

    migrations, design factories

---
+++ src/AuthServerSamples/AuthServerEfCore.DataLayer/ConfigurationContextDesignTimeFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using AuthServerEfCore.DataLayer.Core;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServerEfCore.DataLayer
+{
+    /// <summary>
+    /// Design time factory for <see cref="ConfigurationContext"/>
+    /// </summary>
+    public class ConfigurationContextDesignTimeFactory : DesignTimeDbContextFactory<ConfigurationContext>
+    {
+        protected override string AppSettingsPath => $"{Directory.GetCurrentDirectory()}/../AuthServerEfCore.Web";
+        protected override string ConfigurationName => "Database:ConnectionStrings:Configuration";
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public override ConfigurationContext CreateContext(DbContextOptions<ConfigurationContext> options)
+        {
+            return new ConfigurationContext(options, new ConfigurationStoreOptions());
+        }
+    }
+}
---
+++ src/AuthServerSamples/AuthServerEfCore.DataLayer/PersistedGrantContextDesignTimeFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using AuthServerEfCore.DataLayer.Core;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServerEfCore.DataLayer
+{
+    /// <summary>
+    /// Design time factory for <see cref="PersistedGrantContext"/>
+    /// </summary>
+    public class PersistedGrantContextDesignTimeFactory : DesignTimeDbContextFactory<PersistedGrantContext>
+    {
+        protected override string AppSettingsPath => $"{Directory.GetCurrentDirectory()}/../AuthServerEfCore.Web";
+        protected override string ConfigurationName => "Database:ConnectionStrings:PersistedGrant";
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public override PersistedGrantContext CreateContext(DbContextOptions<PersistedGrantContext> options)
+        {
+            return new PersistedGrantContext(options, new OperationalStoreOptions());
+        }
+    }
+}
