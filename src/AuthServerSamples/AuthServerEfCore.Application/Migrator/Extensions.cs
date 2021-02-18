ee3a0552f1c046a5b3c85632e4cfaa828a7526ea
Садриев Вадим <SadrievVF@it2g.ru>
2021-02-18 14:04:03 +0500

    [EfCore] Seeder for clients

---
+++ src/AuthServerSamples/AuthServerEfCore.Application/Migrator/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServerEfCore.Application.Migrator
+{
+    /// <summary>
+    /// Extension methods for migrations
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Adds or updates entity by predicate
+        /// </summary>
+        public static async Task AddOrUpdateAsync<TEntity>(this DbContext context, TEntity entity, Expression<Func<TEntity, bool>> predicate) where TEntity : class
+        {
+            var dbSet = context.Set<TEntity>();
+
+            var existingEntity = await dbSet.AsNoTracking().FirstOrDefaultAsync(predicate);
+
+            if (existingEntity == null)
+            {
+                dbSet.Add(entity);
+                return;
+            }
+
+            var entityType = context.Model.FindEntityType(typeof(TEntity));
+            var keyFieldInfo = entityType.FindPrimaryKey().Properties.First().FieldInfo;
+
+            var keyValue = keyFieldInfo.GetValue(existingEntity);
+            keyFieldInfo.SetValue(entity, keyValue);
+            dbSet.Update(entity);
+        }
+    }
+}
--- src/AuthServerSamples/AuthServerEfCore.Application/Migrator/Seed/ClientSeeder.cs
+++ src/AuthServerSamples/AuthServerEfCore.Application/Migrator/Seed/ClientSeeder.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using AuthServerEfCore.DataLayer;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.Models;
 
 namespace AuthServerEfCore.Application.Migrator.Seed
 {
@@ -21,9 +24,69 @@ namespace AuthServerEfCore.Application.Migrator.Seed
         /// <summary>
         /// <inheritdoc />
         /// </summary>
-        public Task SeedAsync()
+        public async Task SeedAsync()
         {
-            throw new System.NotImplementedException();
+            var clients = GetClients();
+
+            foreach (var client in clients)
+            {
+                await _context.AddOrUpdateAsync(client.ToEntity(), x => x.ClientId == client.ClientId);
+            }
+
+            var apiScopes = GetApiScopes();
+
+            foreach (var apiScope in apiScopes)
+            {
+                await _context.AddOrUpdateAsync(apiScope.ToEntity(), x => x.Name == apiScope.Name);
+            }
+
+            var identityResources = GetIdentityResources();
+
+            foreach (var identityResource in identityResources)
+            {
+                await _context.AddOrUpdateAsync(identityResource.ToEntity(), x => x.Name == identityResource.Name);
+            }
+
+            var apiResources = GetApiResources();
+
+            foreach (var apiResource in apiResources)
+            {
+                await _context.AddOrUpdateAsync(apiResource.ToEntity(), x => x.Name == apiResource.Name);
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// List of auth server clients to seed
+        /// </summary>
+        private IList<Client> GetClients()
+        {
+            return new List<Client>();
+        }
+
+        /// <summary>
+        /// List of api scopes to seed
+        /// </summary>
+        private IList<ApiScope> GetApiScopes()
+        {
+            return new List<ApiScope>();
+        }
+
+        /// <summary>
+        /// List of identity resources to seed
+        /// </summary>
+        private IList<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>();
+        }
+
+        /// <summary>
+        /// List of api resources to seed
+        /// </summary>
+        private IList<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>();
         }
     }
 }
