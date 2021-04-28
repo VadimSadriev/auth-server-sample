df16656cedb18ba8110b1d5abd651a33ec1fa9c5
Садриев Вадим <SadrievVF@it2g.ru>
2021-04-28 18:47:17 +0500

    Add docker environment

--- run-auth-efcore.sh
+++ run-auth-efcore.sh
@@ -1,4 +1,4 @@
 docker-compose -f docker-compose-efcore.yaml build
-docker rmi -f $(docker images -q --filter label=stage=build)
+sh ./docker-clean-efcore.sh
 docker-compose -f docker-compose-efcore.yaml up -d
 docker ps
