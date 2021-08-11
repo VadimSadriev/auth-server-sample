4f7760d1911c6a4f9c7cd94ef2f3d6f3a4287b35
Anotsu <vadimsadriev.dev@gmail.com>
2021-08-11 23:16:47 +0500

    fix handleLogin with mockUser

--- src/AuthServerSamples/react-app/src/application/contexts/auth/authContextProvider.tsx
+++ src/AuthServerSamples/react-app/src/application/contexts/auth/authContextProvider.tsx
@@ -1,78 +1,82 @@
-import React, { useState, useEffect } from "react";
-import { AuthContext } from "./authContext";
-import { authConfig } from './authContext.Model'
-import { authConfigToUserManagerSettingsMapper } from './authContext.Mappers'
-import { UserManager, UserManagerSettings, User } from "oidc-client";
-import { useHistory } from 'react-router-dom'
+import React, {useState, useEffect} from "react";
+import {AuthContext} from "./authContext";
+import {authConfig} from './authContext.Model'
+import {authConfigToUserManagerSettingsMapper} from './authContext.Mappers'
+import {UserManager, UserManagerSettings, User} from "oidc-client";
+import {useHistory} from 'react-router-dom'
 
 export const AuthProvider: React.FC<authConfig> = (props) => {
 
-  const [user, setUser] = useState<User | null>(null);
-  const userManagerSettings = authConfigToUserManagerSettingsMapper(props)
-  const [userManager, setUserManager] = useState(new UserManager(userManagerSettings));
-  const history = useHistory();
+    const [user, setUser] = useState<User | null>(null);
+    const userManagerSettings = authConfigToUserManagerSettingsMapper(props)
+    const [userManager, setUserManager] = useState(new UserManager(userManagerSettings));
+    const history = useHistory();
 
-  const handleSetUser = (newUser: User) => {
-    setUser(newUser);
-  };
+    const handleSetUser = (newUser: User) => {
+        setUser(newUser);
+    };
 
-  useEffect(() => {
-    initAuthData();
-  }, []);
+    useEffect(() => {
+        initAuthData();
+    }, []);
 
-  const initAuthData = async () => {
+    const initAuthData = async () => {
 
-    if (props.mockUser) {
+        if (props.mockUser) {
 
-      setUser(props.mockUser)
-      return
+            setUser(props.mockUser)
+            return
+        }
+
+        const user = await userManager.getUser();
+
+        if (user)
+            setUser(user);
+    };
+
+    const handleLogin = async () => {
+        try {
+
+            if (props.mockUser) {
+                setUser(props.mockUser)
+                return
+            }
+
+            await userManager.signinRedirect();
+        } catch (err) {
+            console.log(err)
+        }
     }
 
-    const user = await userManager.getUser();
+    const handleLogout = async () => {
+        try {
 
-    if (user)
-      setUser(user);
-  };
+            if (props.mockUser) {
+                setUser(null)
+                history.push('/')
+                return
+            }
 
-  const handleLogin = async () => {
-    try {
-      await userManager.signinRedirect();
+            await userManager.signoutRedirect()
+            setUser(null)
+
+        } catch (err) {
+            console.log(err)
+        }
     }
-    catch (err) {
-      console.log(err)
-    }
-  }
 
-  const handleLogout = async () => {
-    try {
-
-      if (props.mockUser) {
-        setUser(null)
-        history.push('/')
-        return
-      }
-
-      await userManager.signoutRedirect()
-      setUser(null)
-
-    }
-    catch (err) {
-      console.log(err)
-    }
-  }
-
-  return (
-    <AuthContext.Provider
-      value={{
-        user: user,
-        userManager: userManager,
-        setUser: handleSetUser,
-        login: handleLogin,
-        logout: handleLogout,
-        isAuthenticated: user?.expired
-      }}
-    >
-      {props.children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider
+            value={{
+                user: user,
+                userManager: userManager,
+                setUser: handleSetUser,
+                login: handleLogin,
+                logout: handleLogout,
+                isAuthenticated: user?.expired
+            }}
+        >
+            {props.children}
+        </AuthContext.Provider>
+    );
 };
